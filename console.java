b16d91827af69303385e5b94274e642176b394c8
taijiayue1997 <44779618+taijiayue1997@users.noreply.github.com>
2018-11-29 16:11:49 -0500

    Add files via upload

--- console.java
+++ console.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 
 public class console {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		GoFish game = new GoFish();
-		game.gameLoop();
+		boolean play = true; //keep playing?
 		
-		
-		
+		while (play) {
+			GoFish game = new GoFish();
+			game.gameLoop();
+			
+			System.out.println("\n\n\nDo you want to play another round? (y/n): ");
+			Scanner c = new Scanner(System.in);
+			String ans = c.next();
+			while(!ans.equals("y") & !ans.equals("n")) {
+				System.out.println("Please enter y or n: ");
+				ans = c.next();
+			}
+			c.close();
+			
+			play = ans.equals("y");
+			
+			System.out.println("\n\n\n");
+		}
 
-		
+		System.out.println("Bye!");
 	}
 	
 	
